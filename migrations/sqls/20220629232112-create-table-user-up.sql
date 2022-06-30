836e0fac367d6052f10c834e4d792cde52489628
Anthony <anthonynwobodo7@gmail.com>
2022-06-30 15:17:37 +0100

    :sparkles: (postgraphile setup)

--- migrations/sqls/20220629232112-create-table-user-up.sql
+++ migrations/sqls/20220629232112-create-table-user-up.sql
@@ -1,1 +1,9 @@
 /* Replace with your SQL commands */
+CREATE TABLE blog.user (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  email text NOT NULL,
+  password text NOT NULL,
+  created_at timestamptz DEFAULT now(),
+  updated_at timestamptz DEFAULT now()
+);
---
+++ server.sh
@@ -0,0 +1,5 @@
+postgraphile \
+-c postgres://postgres:ElsTony24@localhost:5432/blog_postgraphile \
+--schema blog \
+--watch
+--enhance-graphiql \
