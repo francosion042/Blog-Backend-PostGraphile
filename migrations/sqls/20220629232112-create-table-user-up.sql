f3fe5c2f16c9549277f476539794f10a98217b44
Anthony <anthonynwobodo7@gmail.com>
2022-07-02 18:37:48 +0100

    :sparkles: (new migrations for security)

--- migrations/sqls/20220629232112-create-table-user-up.sql
+++ migrations/sqls/20220629232112-create-table-user-up.sql
@@ -2,6 +2,8 @@
 CREATE TABLE blog.user (
   id serial PRIMARY KEY,
   name text NOT NULL,
+  title text,
+  description text,
   created_at timestamptz DEFAULT now(),
   updated_at timestamptz DEFAULT now()
 );
--- migrations/sqls/20220630161358-comments-user-up.sql
+++ migrations/sqls/20220630161358-comments-user-up.sql
@@ -2,7 +2,5 @@
 COMMENT ON TABLE blog.user IS 'Users of the Application';
 COMMENT ON COLUMN blog.user.id IS 'User ID - Primary Key';
 COMMENT ON COLUMN blog.user.name IS 'Users Display Name';
-COMMENT ON COLUMN blog.user.email IS 'Users Email Address - Unique';
-COMMENT ON COLUMN blog.user.password IS 'Users Password';
 COMMENT ON COLUMN blog.user.created_at IS 'Users Created Date';
 COMMENT ON COLUMN blog.user.updated_at IS 'Users Updated Date';
