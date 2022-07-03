76feedeab5b405317b15d660e31e728f0ce60723
Anthony <anthonynwobodo7@gmail.com>
2022-07-03 18:09:20 +0100

    :sparkles: (user reg function done)

--- migrations/sqls/20220703152411-function-register-user-down.sql
+++ migrations/sqls/20220703152411-function-register-user-down.sql
@@ -1,1 +1,2 @@
 /* Replace with your SQL commands */
+DROP FUNCTION IF EXISTS register_user(name TEXT, email TEXT, password TEXT, title TEXT, description TEXT);
--- migrations/sqls/20220703152411-function-register-user-up.sql
+++ migrations/sqls/20220703152411-function-register-user-up.sql
@@ -1,17 +1,20 @@
 /* Replace with your SQL commands */
-CREATE OR REPLACE FUNCTION register_user(name TEXT, email TEXT, password TEXT, title TEXT, description TEXT) RETURNS blog.user AS 
+CREATE OR REPLACE FUNCTION register_user(name TEXT, email TEXT, password TEXT, title TEXT, description TEXT) RETURNS blog.user AS
 $$
 DECLARE
-    user = blog.user;;
+    userRec blog.user;
 BEGIN
     BEGIN
-        INSERT INTO blog.user (name, title, description) VALUES (name, title, description) RETURNING * INTO user;
+        INSERT INTO blog.user (name, title, description) VALUES (name, title, description) RETURNING * INTO userRec;
 
-        INSERT INTO auth.user_auth (user_id, email, password) VALUES (user.id, email, crypt(password, gen_salt('bf')));
-        RETURN user;
-    EXCEPTION WHEN unique_violation THEN
-        RAISE WARNING 'User with this email already exists';
+        INSERT INTO auth.user_auth (user_id, email, password) VALUES (userRec.id, email, crypt(password, gen_salt('bf')));
+        RETURN userRec;
+        EXCEPTION WHEN unique_violation THEN
+            RAISE WARNING 'User with this email already exists';
         RETURN NULL;
     END;
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql
+STRICT
+SECURITY DEFINER;
