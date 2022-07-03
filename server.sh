9c5b573dda20e3fa4fd630c5a43ed8c1abae7cfc
Anthony <anthonynwobodo7@gmail.com>
2022-07-03 22:50:52 +0100

    :sparkles: (roles config done)

--- server.sh
+++ server.sh
@@ -1,7 +1,9 @@
 postgraphile \
--c postgres://postgres:ElsTony24@localhost:5432/blog_postgraphile \
+-c postgres://blog_admin:AdminPassword@localhost:5432/blog_postgraphile \
 --schema blog \
---watch \
 --enhance-graphiql \
 --allow-explain \
 --append-plugins @graphile-contrib/pg-simplify-inflector \
+--owner-connection postgres://postgres:ElsTony24@localhost:5432/blog_postgraphile \
+--default-role blog_guest \
+--watch \
