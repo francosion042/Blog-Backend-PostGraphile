c028bea964e3592521aac676b53bb07c1927e29f
Anthony <anthonynwobodo7@gmail.com>
2022-07-04 09:50:21 +0100

    :bug: (down migrations fix)

--- migrations/sqls/20220703203656-create-roles-down.sql
+++ migrations/sqls/20220703203656-create-roles-down.sql
@@ -1,4 +1,7 @@
 /* Replace with your SQL commands */
+REASSIGN OWNED BY blog_admin TO postgres;
+DROP OWNED BY blog_admin;
+REASSIGN OWNED BY blog_user TO postgres;
 DROP ROLE blog_admin;
 DROP ROLE blog_user;
 DROP ROLE blog_guest;
